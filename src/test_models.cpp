17f4534048c8867459794b9d1a5aa29cb8be994d
icirauqui <icirauqui@gmail.com>
2024-01-11 07:32:48 +0100

    new model

--- src/test_models.cpp
+++ src/test_models.cpp
@@ -5,7 +5,6 @@ class AbaqusC3D8_1 {
   public:
     AbaqusC3D8_1() {
       build_nodes();
-      //build_elements();
     }
     
     ~AbaqusC3D8_1() {}
@@ -16,8 +15,25 @@ class AbaqusC3D8_1 {
       for (int x=int(_x0); x>=0; x-=int(_w)) {
         for (int z=int(_z0); z>=0; z-=int(_w)) {
           for (int y=int(_y0); y>=0; y-=int(_w), n++) {
-            std::cout << "n = " << n << " : (x,y,z) = (" << x << "," << y << "," << z << ")" << std::endl;
+            //std::cout << "n = " << n << " : (x,y,z) = (" << x << "," << y << "," << z << ")" << std::endl;
             _nodes.push_back(Eigen::Vector3d(double(x),double(y),double(z)));
+
+            if (x==0 || y==0 || z==0) continue;
+
+            unsigned int offset = (_y0+1)*(_z0+1);//*(_x0+1-x);
+            //std::cout << "n (offset) = " << n << " (" << offset << ") : (x,y,z) = (" << x << "," << y << "," << z << ")" << std::endl;
+
+            std::array<unsigned int,8> element = {
+              n,
+              n+1,
+              n+1+(int(_y0)+1),
+              n+(int(_y0)+1),
+              n+offset,
+              n+1+offset,
+              n+1+(int(_y0)+1)+offset,
+              n+(int(_y0)+1)+offset
+            };
+            _elements.push_back(element);
           }
         }
       }  
@@ -25,15 +41,33 @@ class AbaqusC3D8_1 {
 
     void build_elements() {
       unsigned int n = 0;
+      unsigned int elt = 0;
+      unsigned int offset = 0;
       
       for (int x=int(_x0); x>=0; x-=int(_w)) {
         for (int z=int(_z0); z>=0; z-=int(_w)) {
           for (int y=int(_y0); y>=0; y-=int(_w), n++) {
             if (x==0 || y==0 || z==0) continue;
+            offset = (_y0+1)*(_z0+1);//*(_x0+1-x);
+            std::cout << "n (offset) = " << n << " (" << offset << ") : (x,y,z) = (" << x << "," << y << "," << z << ")" << std::endl;
 
-            std::cout << "n = " << n << " : (x,y,z) = (" << x << "," << y << "," << z << ")" << std::endl;
             std::array<unsigned int,8> element;
-            //element[0] = 
+            element[0] = n;
+            element[1] = n+1;
+            element[2] = n+1+(_y0+1);
+            element[3] = n+(_y0+1);
+            element[4] = n+offset;
+            element[5] = n+1+offset;
+            element[6] = n+1+(_y0+1)+offset;
+            element[7] = n+(_y0+1)+offset;
+            _elements.push_back(element);
+            elt++;
+            //std::cout << "elt = " << elt << " : ";
+            //for (int i=0; i<8; i++) {
+            //  std::cout << element[i] << " ";
+            //}
+            //std::cout << std::endl << std::endl;
+
             
           }
         }
